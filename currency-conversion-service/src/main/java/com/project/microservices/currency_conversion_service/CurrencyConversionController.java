f6bc8881b2d47795269a2190fbc8f9afd3481f07
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2024-07-07 10:00:30 +0530

    Connecting Currency Conversion Microservice & API Gateway with Zipkin

--- currency-conversion-service/src/main/java/com/project/microservices/currency_conversion_service/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/project/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -2,6 +2,9 @@ package com.project.microservices.currency_conversion_service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +14,23 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
@@ -24,7 +39,7 @@ public class CurrencyConversionController {
         uriVariables.put("from", from);
         uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity(
                 "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
                 CurrencyConversion.class, uriVariables);
 
